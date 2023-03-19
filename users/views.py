9d565758e1c05e1f14f34bd19ce17746f1d29e5e
Andre Araujo <andre.araujosilva2006@gmail.com>
2023-03-19 19:23:15 -0300

    correção css

--- users/views.py
+++ users/views.py
@@ -13,8 +13,37 @@ from django.shortcuts import redirect
 #se o email é um email
 
 def signup(request):
-    if request.method == "GET":
+    if request.method == 'GET':
         return render(request, 'signup.html')
+    elif request.method == 'POST':
+        form = {
+            'username': request.POST.get('username'),
+            'email': request.POST.get('email'),
+            'password': request.POST.get('password')
+        }
+
+        if any([len(form[item].strip()) == 0 for item in form]):
+            messages.add_message(request, constants.WARNING, 'Preencha todos os campos corretamente')
+            return render(request, 'signup.html')
+        if len(form['password']) < 6:
+            messages.add_message(request, constants.WARNING, 'Insira uma senha com mais de seis dígitos')
+            return render(request, 'signup.html')
+        
+        try:    
+            user = User.objects.create_user(
+                username=form['username'],
+                email=form['email'],
+                password=form['password']
+            )
+
+            messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso')
+            return render(request, 'signup.html')
+        except IntegrityError:
+            messages.add_message(request, constants.ERROR, 'Nome de usuário ja cadastrado')
+            return render(request, 'signup.html')
+        except:
+            messages.add_message(request, constants.ERROR, 'Ocorreu um erro inesperado, tente novamente mais tarde X_X')
+            return render(request, 'signup.html')  
 
 """
     if request.user.is_authenticated:
@@ -58,14 +87,14 @@ def signup(request):
 
 def logar(request):
     if request.user.is_authenticated:
-        return redirect('/posts/new_post')
+        return redirect('/checklist/')
 
 
     if request.method == "GET":
         return render(request, 'login.html')
     elif request.method == "POST":
         data = {
-            "name": request.POST.get('name'),
+            "username": request.POST.get('username'),
             "password": request.POST.get('password'),
         }    
         
